d083242470c8e3df5553b33b0d69b76a6922d9df
Fede Diaz <nordri@gmail.com>
2019-07-18 11:44:05 +0200

    More output and check where i am

--- roles/healthcheck/files/openvidu_healthcheck.sh
+++ roles/healthcheck/files/openvidu_healthcheck.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# Check where i am
+MEMBER=$(cat /opt/openvidu/openvidu-cluster-member)
+
+if [ "$MEMBER" == "server" ]; then
+
 # Check Docker
 DOCKER=$(ps ax | grep /usr/bin/dockerd)
 if [ "x${DOCKER}" == "x" ]; then
 	echo "[ERROR] DOCKER is not running..."
 	exit 1
+else
+	echo "[OK] DOCKER is running..."
 fi
 
 # Check NGiNX
@@ -13,6 +20,8 @@ NGINX=$(ps ax | grep nginx)
 if [ "x${NGINX}" == "x" ]; then
 	echo "[ERROR] NGINX is not running..."
 	exit 1
+else
+	echo "[OK] NGINX is running..."
 fi
 
 # Check Coturn
@@ -20,6 +29,8 @@ COTURN=$(ps ax | grep coturn)
 if [ "x${COTURN}" == "x" ]; then
 	echo "[ERROR] COTURN is not running..."
 	exit 1
+else
+	echo "[OK] COTURN is running..."
 fi
 
 # Check Openvidu
@@ -27,6 +38,8 @@ OPENVIDU=$(ps ax | grep openvidu-server)
 if [ "x${OPENVIDU}" == "x" ]; then
 	echo "[ERROR] OPENVIDU is not running..."
 	exit 1
+else
+	echo "[OK] OPENVIDU is running..."	
 fi
 
 # Check Elastic
@@ -34,10 +47,35 @@ ELASTICSEARCH=$(ps ax | grep elasticsearch)
 if [ "x${ELASTICSEARCH}" == "x" ]; then
 	echo "[ERROR] ELASTICSEARCH is not running..."
 	exit 1
+else
+	echo "[OK] ELASTICSEARCH is running..."
 fi
 
 KIBANA=$(ps ax | grep elasticsearch)
 if [ "x${KIBANA}" == "x" ]; then
 	echo "[ERROR] KIBANA is not running..."
 	exit 1
+else
+	echo "[OK] KIBANA is running..."
+fi
+
+else
+
+KMS=$(ps ax | grep kurento-media-server)
+if [ "x${KMS}" == "x" ]; then
+	echo "[ERROR] KMS is not running..."
+	exit 1
+else
+	echo "[OK] KMS is running..."
+fi
+
+# Check NGiNX
+NGINX=$(ps ax | grep nginx)
+if [ "x${NGINX}" == "x" ]; then
+	echo "[ERROR] NGINX is not running..."
+	exit 1
+else
+	echo "[OK] NGINX is running..."
+fi
+
 fi
